2bf587ade649e1d51eec701673d31dfee395598b
georgearmando <georgearmando54@gmail.com>
2023-09-23 19:27:02 +0000

    change implement repository for category repo

--- src/category/domain/category.repository.ts
+++ src/category/domain/category.repository.ts
@@ -1,5 +1,18 @@
-import { IRepository } from "../../shared/domain/repositories/repository-interface";
+import { ISearchableRepository } from "../../shared/domain/repositories/repository-interface";
+import { SearchParams } from "../../shared/domain/repositories/search-params";
+import { SearchResult } from "../../shared/domain/repositories/search-result";
 import { Uuid } from "../../shared/domain/value-objects/uuid.vo";
 import { Category } from "./category.entity";
 
-export interface ICategoryRepository extends IRepository<Category, Uuid> {}
+export type CategoryFilter = string;
+export class CategorySearchParams extends SearchParams<CategoryFilter>{}
+export class CategorySearchResult extends SearchResult<Category>{}
+
+export interface ICategoryRepository 
+  extends ISearchableRepository<
+  Category, 
+  Uuid,
+  CategoryFilter,
+  CategorySearchParams,
+  CategorySearchResult
+  > {}
