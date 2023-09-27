eebcd208edcd08bf25dc06c96ba234d231d52991
georgearmando <georgearmando54@gmail.com>
2023-09-27 14:42:56 +0000

    create category output mapper unit tests

---
+++ src/category/application/use-cases/commom/category-output.spec.ts
@@ -0,0 +1,23 @@
+import { Category } from "../../../domain/category.entity";
+import { CategoryOutputMapper } from "./category-output";
+
+describe("CategoryOutputMapper Unit Tests", () => {
+  it("should return a category output", () => {
+    const entity = Category.create({
+      name: "Movie",
+      description: "some description",
+      is_active: true
+    });
+
+    const spyToJSON = jest.spyOn(entity, "toJSON");
+    const output = CategoryOutputMapper.toOutput(entity);
+    expect(spyToJSON).toHaveBeenCalledTimes(1);
+    expect(output).toStrictEqual({
+      id: entity.category_id.id,
+      name: "Movie",
+      description: "some description",
+      is_active: true,
+      created_at: entity.created_at,
+    });
+  });
+});
