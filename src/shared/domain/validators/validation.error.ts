7cbdbe40900a0e69b6fd98504166b30464373b57
georgearmando <georgearmando54@gmail.com>
2023-09-28 12:24:46 +0000

    add array notation to FieldErrors

--- src/shared/domain/validators/validation.error.ts
+++ src/shared/domain/validators/validation.error.ts
@@ -3,7 +3,7 @@ import { FieldsErrors } from "../validators/validator-field-interface";
 export class ValidationError extends Error {}
 
 export class EntityValidationError extends Error {
-  constructor(public error: FieldsErrors, message = "Validation Error") {
+  constructor(public error: FieldsErrors[], message = "Entity Validation Error") {
     super(message);
     this.name = "EntityValidationError";
   }
