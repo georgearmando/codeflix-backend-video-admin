a57b668fdcbaac769f7bef9370a30a6b55e71516
georgearmando <georgearmando54@gmail.com>
2023-09-28 12:09:58 +0000

    add notification error to jest Matcher interface

--- src/jest.d.ts
+++ src/jest.d.ts
@@ -4,6 +4,9 @@ declare global {
   namespace jest {
     interface Matchers<R> {
       containsErrorMessages: (expected: FieldsErrors) => R;
+      notificationContainsErrorMessages: (
+        expected: Array<string | { [key: string]: string[] }>,
+      ) => R;
     }
   }
 }
